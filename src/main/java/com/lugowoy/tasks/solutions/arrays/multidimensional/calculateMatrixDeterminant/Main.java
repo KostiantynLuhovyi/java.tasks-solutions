18a0a979435737c1385a63966ed0734f767dbfbb
Konstantin <kostya.lugowoy@gmail.com>
2020-12-20 20:45:46 +0200

    fix(arrays) Fixing code in the ../multidimensional/calculateMatrixDeterminant package with a solution of the task

--- src/main/java/com/lugowoy/tasks/solutions/arrays/multidimensional/calculateMatrixDeterminant/Main.java
+++ src/main/java/com/lugowoy/tasks/solutions/arrays/multidimensional/calculateMatrixDeterminant/Main.java
@@ -1,65 +1,91 @@
 package com.lugowoy.tasks.solutions.arrays.multidimensional.calculateMatrixDeterminant;
 
+import com.lugowoy.helper.execution.OutputExecutionResultToConsole;
+import com.lugowoy.helper.execution.OutputExecutionTimeToConsole;
+import com.lugowoy.helper.filling.matrix.numbers.primitives.FillingMatrixRandomPrimitiveIntegers;
 import com.lugowoy.helper.models.matrices.MatrixInts;
-import com.lugowoy.helper.utils.execution.ExecutionResultOutputToConsole;
-import com.lugowoy.helper.utils.execution.ExecutionTimeOutputToConsole;
-import com.lugowoy.helper.utils.execution.Executor;
-import com.lugowoy.tasks.solutions.Helper;
+import com.lugowoy.tasks.solutions.helper.Helper;
+import com.lugowoy.tasks.solutions.helper.HelperFiller;
+import org.jetbrains.annotations.NotNull;
 
-import static com.lugowoy.helper.filling.ValuesToFilling.INT_LOWER_BOUND;
-import static com.lugowoy.helper.filling.ValuesToFilling.INT_UPPER_BOUND;
+import java.util.Objects;
+
 import static java.lang.Math.pow;
 
 /**
  * Calculate the matrix determinant.
- * <p> Created by Konstantin Lugowoy on 31.10.2018.
+ *
+ * <p>Created by Konstantin Lugowoy on 31.10.2018.
  */
 public class Main {
 
-    private static final String MSG_OUTPUT_RESULT = "Matrix determinant : %.3f\n";
+    private static final String MSG_DETERMINANT_RESULT =
+            "Matrix determinant : %.3f\n";
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
-        int rows = Helper.enterMatrixRowCountToConsole();
-        int columns = Helper.enterMatrixColumnCountToConsole();
+        int rows = Helper.readConsoleMatrixRows();
+        int columns = Helper.readConsoleMatrixColumns();
 
-        MatrixInts matrix = new MatrixInts(Helper.FILLING_MATRIX_INTS.fill(rows, columns, INT_LOWER_BOUND, INT_UPPER_BOUND));
+        MatrixInts matrix = new MatrixInts(new HelperFiller().getArray(
+                new FillingMatrixRandomPrimitiveIntegers(), rows, columns,
+                HelperFiller.INT_NEGATIVE_THOUSAND,
+                HelperFiller.INT_POSITIVE_THOUSAND));
 
-        System.out.println(matrix);
-        System.out.println();
+        System.out.println(matrix + "\n");
 
-        Executor.execute(() -> calculateMatrixDeterminant(matrix),
-                                      ExecutionResultOutputToConsole::outputExecutionResultToConsole, MSG_OUTPUT_RESULT,
-                                      ExecutionTimeOutputToConsole::outputExecutionTime);
+        Helper.EXECUTOR.execute(() -> calculateDeterminantOfMatrix(matrix),
+                                new OutputExecutionResultToConsole(),
+                                MSG_DETERMINANT_RESULT,
+                                new OutputExecutionTimeToConsole());
 
     }
 
-    private static double calculateMatrixDeterminant(MatrixInts matrix) {
+    private static double calculateDeterminantOfMatrix(
+            @NotNull final MatrixInts matrix) {
+        Objects.requireNonNull(matrix, "Matrix is null");
         MatrixInts tmpMatrix;
         double resultMatrixDeterminant = 0;
-        if (matrix.getRows() == 1) {
-            resultMatrixDeterminant = matrix.getElement(0, 0);
-            return resultMatrixDeterminant;
-        }
-        if (matrix.getRows() == 2) {
-            resultMatrixDeterminant = (matrix.getElement(0, 0) * matrix.getElement(1, 1))
-                                            - (matrix.getElement(0, 1) * matrix.getElement(1, 0));
-            return resultMatrixDeterminant;
-        }
+        resultMatrixDeterminant = calculateDeterminantMatrixWithOneRow(matrix);
+        resultMatrixDeterminant = calculateDeterminantMatrixWithTwoRows(matrix);
         for (int i = 0; i < matrix.getRows(); i++) {
-            tmpMatrix = new MatrixInts(matrix.getRows() - 1, matrix.getColumns() - 1);
+            tmpMatrix = new MatrixInts(matrix.getRows() - 1,
+                                       matrix.getColumns() - 1);
             for (int j = 1; j < matrix.getRows(); j++) {
                 for (int k = 0; k < matrix.getColumns(); k++) {
                     if (k < i) {
                         tmpMatrix.setElement(j - 1, k, matrix.getElement(j, k));
                     } else if (k > i) {
-                        tmpMatrix.setElement(j - 1, k - 1, matrix.getElement(j, k));
+                        tmpMatrix.setElement(j - 1, k - 1,
+                                             matrix.getElement(j, k));
                     }
                 }
             }
-            resultMatrixDeterminant += matrix.getElement(0, i) * pow(-1, i) * calculateMatrixDeterminant(tmpMatrix);
+            resultMatrixDeterminant += matrix.getElement(0, i) * pow(-1, i)
+                                       * calculateDeterminantOfMatrix(tmpMatrix);
         }
         return resultMatrixDeterminant;
+    }
+
+    private static double calculateDeterminantMatrixWithOneRow(
+            @NotNull final MatrixInts matrix) {
+        Objects.requireNonNull(matrix, "Matrix is null");
+        double result = 0;
+        if (matrix.getRows() == 1) {
+            result = matrix.getElement(0, 0);
+        }
+        return result;
+    }
+
+    private static double calculateDeterminantMatrixWithTwoRows(
+            @NotNull final MatrixInts matrix) {
+        Objects.requireNonNull(matrix, "Matrix is null");
+        double result = 0;
+        if (matrix.getRows() == 2) {
+            result = (matrix.getElement(0, 0) * matrix.getElement(1, 1)) - (
+                    matrix.getElement(0, 1) * matrix.getElement(1, 0));
+        }
+        return result;
     }
 
 }
