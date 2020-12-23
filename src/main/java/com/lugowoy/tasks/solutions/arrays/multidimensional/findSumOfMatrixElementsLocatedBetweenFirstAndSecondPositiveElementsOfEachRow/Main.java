454ca70eb81eda3a6d31e2074e202d55fe8e3292
Konstantin <kostya.lugowoy@gmail.com>
2020-12-23 22:49:48 +0200

    fix(array) Fixing code in the ../multidimensional/findSumOfMatrixElementsLocatedBetweenFirstAndSecondPositiveElementsOfEachRow package with a solution

--- src/main/java/com/lugowoy/tasks/solutions/arrays/multidimensional/findSumOfMatrixElementsLocatedBetweenFirstAndSecondPositiveElementsOfEachRow/Main.java
+++ src/main/java/com/lugowoy/tasks/solutions/arrays/multidimensional/findSumOfMatrixElementsLocatedBetweenFirstAndSecondPositiveElementsOfEachRow/Main.java
@@ -1,39 +1,47 @@
 package com.lugowoy.tasks.solutions.arrays.multidimensional.findSumOfMatrixElementsLocatedBetweenFirstAndSecondPositiveElementsOfEachRow;
 
+import com.lugowoy.helper.execution.OutputExecutionResultToConsole;
+import com.lugowoy.helper.execution.OutputExecutionTimeToConsole;
+import com.lugowoy.helper.filling.matrix.numbers.primitives.FillingMatrixRandomPrimitiveIntegers;
 import com.lugowoy.helper.models.matrices.MatrixInts;
-import com.lugowoy.helper.utils.execution.ExecutionResultOutputToConsole;
-import com.lugowoy.helper.utils.execution.ExecutionTimeOutputToConsole;
-import com.lugowoy.helper.utils.execution.Executor;
-import com.lugowoy.tasks.solutions.Helper;
+import com.lugowoy.tasks.solutions.helper.Helper;
+import com.lugowoy.tasks.solutions.helper.HelperFiller;
+import org.jetbrains.annotations.NotNull;
 
-import static com.lugowoy.helper.filling.ValuesToFilling.INT_LOWER_BOUND;
-import static com.lugowoy.helper.filling.ValuesToFilling.INT_UPPER_BOUND;
+import java.util.Objects;
 
 /**
- * Find the sum of matrix elements located between the first and second positive elements of each row.
- * <p> Created by Konstantin Lugowoy on 29.10.2018.
+ * Find the sum of matrix elements located between the first and second positive
+ * elements of each row.
+ *
+ * <p>Created by Konstantin Lugowoy on 29.10.2018.
  */
 public class Main {
 
-    private static final String RESULT_SUM_ELEMENTS = "Result sum elements : %d";
+    private static final String RESULT_SUM_ELEMENTS =
+            "Result sum elements : %d";
 
     public static void main(String[] args) throws Exception {
 
-        int rows = Helper.enterMatrixRowCountToConsole();
-        int columns = Helper.enterMatrixColumnCountToConsole();
+        int rows = Helper.readConsoleMatrixRows();
+        int columns = Helper.readConsoleMatrixColumns();
 
-        MatrixInts matrix = new MatrixInts(Helper.FILLING_MATRIX_INTS.fill(rows, columns, INT_LOWER_BOUND, INT_UPPER_BOUND));
+        MatrixInts matrix = new MatrixInts(new HelperFiller().getArray(
+                new FillingMatrixRandomPrimitiveIntegers(), rows, columns,
+                HelperFiller.INT_NEGATIVE_TEN, HelperFiller.INT_POSITIVE_TEN));
 
-        System.out.println("Matrix : ");
-        System.out.println(matrix);
+        System.out.println("Matrix : \n" + matrix + "\n");
 
-        Executor.execute(() -> findSumOfMatrixElementsLocatedBetweenFirstAndSecondPositiveElementsOfEachRow(matrix),
-                                    ExecutionResultOutputToConsole::outputExecutionResultToConsole, RESULT_SUM_ELEMENTS,
-                                    ExecutionTimeOutputToConsole::outputExecutionTime);
+        Helper.EXECUTOR.execute(() -> findSumOfMatrixElements(matrix),
+                                new OutputExecutionResultToConsole(),
+                                RESULT_SUM_ELEMENTS,
+                                new OutputExecutionTimeToConsole());
 
     }
 
-    private static int findSumOfMatrixElementsLocatedBetweenFirstAndSecondPositiveElementsOfEachRow(MatrixInts matrix) {
+    private static int findSumOfMatrixElements(
+            @NotNull final MatrixInts matrix) {
+        Objects.requireNonNull(matrix, "Matrix is null");
         int tmpSum = 0;
         int sumAllElements = 0;
         boolean firstPositive = false;
