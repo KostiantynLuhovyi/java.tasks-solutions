9721008be16d87fd3e685928d7b1a70b6f232e28
Konstantin <kostya.lugowoy@gmail.com>
2020-12-24 15:58:32 +0200

    fix(array) Fixing code in the ../multidimensional/rebuildMatrixRearrangingRowsInItSoThatElementsInRowsOfResultingMatrixAreIncreased package with a solution of the task

--- src/main/java/com/lugowoy/tasks/solutions/arrays/multidimensional/rebuildMatrixRearrangingRowsInItSoThatElementsInRowsOfResultingMatrixAreIncreased/Main.java
+++ src/main/java/com/lugowoy/tasks/solutions/arrays/multidimensional/rebuildMatrixRearrangingRowsInItSoThatElementsInRowsOfResultingMatrixAreIncreased/Main.java
@@ -1,37 +1,44 @@
 package com.lugowoy.tasks.solutions.arrays.multidimensional.rebuildMatrixRearrangingRowsInItSoThatElementsInRowsOfResultingMatrixAreIncreased;
 
+import com.lugowoy.helper.execution.OutputExecutionResultToConsole;
+import com.lugowoy.helper.execution.OutputExecutionTimeToConsole;
+import com.lugowoy.helper.filling.matrix.numbers.primitives.FillingMatrixRandomPrimitiveIntegers;
 import com.lugowoy.helper.models.matrices.MatrixInts;
-import com.lugowoy.helper.utils.execution.ExecutionResultOutputToConsole;
-import com.lugowoy.helper.utils.execution.ExecutionTimeOutputToConsole;
-import com.lugowoy.helper.utils.execution.Executor;
-import com.lugowoy.tasks.solutions.Helper;
+import com.lugowoy.tasks.solutions.helper.Helper;
+import com.lugowoy.tasks.solutions.helper.HelperFiller;
+import org.jetbrains.annotations.NotNull;
 
-import static com.lugowoy.tasks.solutions.Helper.RESULT_MATRIX;
+import java.util.Objects;
+
+import static com.lugowoy.tasks.solutions.helper.Helper.RESULT_MATRIX;
 
 /**
- * Rebuild the matrix, rearranging the rows in it so that the elements in the rows of the resulting matrix are increased.
- * <p> Created by Konstantin Lugowoy on 23.11.2018.
+ * Rebuild the matrix, rearranging the rows in it so that the elements in the
+ * rows of the resulting matrix are increased.
+ *
+ * <p>Created by Konstantin Lugowoy on 23.11.2018.
  */
 public class Main {
 
-    private static final int BOUND = 100;
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws Exception {
+        int rows = Helper.readConsoleMatrixRows();
+        int columns = Helper.readConsoleMatrixColumns();
 
-        int rows = Helper.enterMatrixRowCountToConsole();
-        int columns = Helper.enterMatrixColumnCountToConsole();
+        MatrixInts matrix = new MatrixInts(new HelperFiller().getArray(
+                new FillingMatrixRandomPrimitiveIntegers(), rows, columns,
+                HelperFiller.INT_ZERO, HelperFiller.INT_POSITIVE_HUNDRED));
 
-        MatrixInts matrix = new MatrixInts(Helper.FILLING_MATRIX_INTS.fill(rows, columns, BOUND));
+        System.out.println("Matrix : \n" + matrix + "\n");
 
-        System.out.println("Original matrix : ");
-        System.out.println(matrix);
-
-        Executor.execute(() -> rebuildMatrix(matrix), ExecutionResultOutputToConsole::outputExecutionResultToConsole, RESULT_MATRIX,
-                                                      ExecutionTimeOutputToConsole::outputExecutionTime);
+        Helper.EXECUTOR.execute(() -> rebuildMatrix(matrix),
+                                new OutputExecutionResultToConsole(), RESULT_MATRIX,
+                                new OutputExecutionTimeToConsole());
 
     }
 
-    private static MatrixInts rebuildMatrix(MatrixInts matrix) {
+    private static MatrixInts rebuildMatrix(@NotNull final MatrixInts matrix) {
+        Objects.requireNonNull(matrix, "Matrix is null");
         int[][] matrixSort = matrix.toMatrix(new int[matrix.getRows()][matrix.getColumns()]);
         for (int i = 0; i < matrix.getRows(); i++) {
             for (int j = 0; j < matrix.getColumns() - i - 1; j++) {
