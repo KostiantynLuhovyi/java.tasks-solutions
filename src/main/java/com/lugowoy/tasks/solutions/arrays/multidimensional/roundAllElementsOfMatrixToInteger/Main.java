1f56af7c34f5aa99104538a91de1373a920014ff
Konstantin <kostya.lugowoy@gmail.com>
2020-12-24 21:13:51 +0200

    fix(array) Fixing code in the ../multidimensional/roundAllElementsOfMatrixToInteger package with a solution of the task

--- src/main/java/com/lugowoy/tasks/solutions/arrays/multidimensional/roundAllElementsOfMatrixToInteger/Main.java
+++ src/main/java/com/lugowoy/tasks/solutions/arrays/multidimensional/roundAllElementsOfMatrixToInteger/Main.java
@@ -1,12 +1,16 @@
 package com.lugowoy.tasks.solutions.arrays.multidimensional.roundAllElementsOfMatrixToInteger;
 
+import com.lugowoy.helper.execution.OutputExecutionTimeToConsole;
+import com.lugowoy.helper.filling.matrix.numbers.primitives.FillingMatrixRandomPrimitiveDoubles;
 import com.lugowoy.helper.models.matrices.MatrixDoubles;
 import com.lugowoy.helper.models.matrices.MatrixInts;
-import com.lugowoy.tasks.solutions.Helper;
+import com.lugowoy.tasks.solutions.helper.Helper;
+import com.lugowoy.tasks.solutions.helper.HelperFiller;
 
 /**
  * Round all elements of the matrix to an integer.
- * <p> Created by Konstantin Lugowoy on 07.11.2018.
+ *
+ * <p>Created by Konstantin Lugowoy on 07.11.2018.
  */
 public class Main {
 
@@ -14,23 +18,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int rows = Helper.enterMatrixRowCountToConsole();
-        int columns = Helper.enterMatrixColumnCountToConsole();
+        int rows = Helper.readConsoleMatrixRows();
+        int columns = Helper.readConsoleMatrixColumns();
 
-        MatrixDoubles matrix = new MatrixDoubles(Helper.FILLING_MATRIX_DOUBLES.fill(rows, columns, BOUND));
+        MatrixDoubles matrix = new MatrixDoubles(new HelperFiller().getArray(
+                new FillingMatrixRandomPrimitiveDoubles(), rows, columns,
+                HelperFiller.DOUBLE_ZERO, BOUND));
 
-        System.out.println("Original matrix : ");
-        System.out.println(matrix);
+        System.out.println("Matrix : \n" + matrix + "\n");
 
-        MatrixInts integerMatrix = new MatrixInts(matrix.getRows(), matrix.getColumns());
-        for (int i = 0; i < matrix.getRows(); i++) {
-            for (int j = 0; j < matrix.getColumns(); j++) {
-                integerMatrix.setElement(i, j, (int) Math.round(matrix.getElement(i, j)));
+        Helper.EXECUTOR.execute(() -> {
+            MatrixInts integerMatrix = new MatrixInts(matrix.getRows(), matrix.getColumns());
+            for (int i = 0; i < matrix.getRows(); i++) {
+                for (int j = 0; j < matrix.getColumns(); j++) {
+                    integerMatrix.setElement(i, j, (int) Math.round(matrix.getElement(i, j)));
+                }
             }
-        }
-
-        System.out.println("Result matrix : ");
-        System.out.println(integerMatrix);
+            System.out.println("Result matrix : \n");
+            System.out.println(integerMatrix);
+        }, new OutputExecutionTimeToConsole());
 
     }
 
