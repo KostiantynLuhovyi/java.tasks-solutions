d98418450c054d2514f404d191dfec1aa829197d
Konstantin <kostya.lugowoy@gmail.com>
2020-12-24 21:26:08 +0200

    fix(array) Fixing code in the ../multidimensional/secondTaskForFormationOfSquareMatrixAccordingToGivenPattern package with a solution of the task

--- src/main/java/com/lugowoy/tasks/solutions/arrays/multidimensional/secondTaskForFormationOfSquareMatrixAccordingToGivenPattern/Main.java
+++ src/main/java/com/lugowoy/tasks/solutions/arrays/multidimensional/secondTaskForFormationOfSquareMatrixAccordingToGivenPattern/Main.java
@@ -1,7 +1,8 @@
 package com.lugowoy.tasks.solutions.arrays.multidimensional.secondTaskForFormationOfSquareMatrixAccordingToGivenPattern;
 
-import com.lugowoy.helper.io.reading.ReadingConsole;
-import com.lugowoy.helper.utils.ArrayLengthReader;
+import com.lugowoy.helper.execution.OutputExecutionTimeToConsole;
+import com.lugowoy.helper.utils.ReaderConsole;
+import com.lugowoy.tasks.solutions.helper.Helper;
 
 /**
  * Form a square matrix of order n according to a given pattern:
@@ -11,7 +12,8 @@ import com.lugowoy.helper.utils.ArrayLengthReader;
  * …	…	…	…	…	…	…
  * 0	n-1	0	…	0	0	0
  * n	0	0	…	0	0	0
- * <p> Created by Konstantin Lugowoy on 16.10.2017.
+ *
+ * <p>Created by Konstantin Lugowoy on 16.10.2017.
  */
 public class Main {
 
@@ -20,24 +22,26 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Enter the length of the matrix elements : ");
-        int n = ArrayLengthReader.readLength(new ReadingConsole(), UPPER_BOUND);
+        int n = new ReaderConsole().readInt(0, UPPER_BOUND);
 
         int[][] array = new int[n][n];
 
-        int indexNum = array.length - 1, num = 1;
-        for (int i = 0; i < array.length; i++) {
-            array[i][indexNum] = num;
-            num++;
-            indexNum--;
-        }
-
-        System.out.println("Result matrix  : ");
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                System.out.print(array[i][j] + " ");
+        Helper.EXECUTOR.execute(() -> {
+            int indexNum = array.length - 1, num = 1;
+            for (int i = 0; i < array.length; i++) {
+                array[i][indexNum] = num;
+                num++;
+                indexNum--;
             }
-            System.out.println();
-        }
+
+            System.out.println("Result matrix  : \n");
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array[i].length; j++) {
+                    System.out.print(array[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }, new OutputExecutionTimeToConsole());
 
     }
 
