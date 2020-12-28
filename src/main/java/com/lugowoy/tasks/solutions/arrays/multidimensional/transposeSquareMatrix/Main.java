511c642408b534fdc1e24b90df7312e064d1426f
Konstantin <kostya.lugowoy@gmail.com>
2020-12-28 16:55:56 +0200

    fix(array) Fixing code in the ../multidimensional/transposeSquareMatrix package with a solution of the task

--- src/main/java/com/lugowoy/tasks/solutions/arrays/multidimensional/transposeSquareMatrix/Main.java
+++ src/main/java/com/lugowoy/tasks/solutions/arrays/multidimensional/transposeSquareMatrix/Main.java
@@ -1,36 +1,40 @@
 package com.lugowoy.tasks.solutions.arrays.multidimensional.transposeSquareMatrix;
 
-import com.lugowoy.helper.filling.matrix.numbers.FillingMatrixRandomPrimitiveIntegers;
+import com.lugowoy.helper.execution.OutputExecutionTimeToConsole;
+import com.lugowoy.helper.filling.matrix.numbers.primitives.FillingMatrixRandomPrimitiveIntegers;
+import com.lugowoy.helper.models.matrices.AbstractMatrix;
 import com.lugowoy.helper.models.matrices.MatrixInts;
-import com.lugowoy.tasks.solutions.Helper;
-
-import static com.lugowoy.helper.filling.ValuesToFilling.INT_UPPER_BOUND;
-import static com.lugowoy.helper.models.matrices.AbstractMatrix.DEFAULT_COLUMNS;
-import static com.lugowoy.helper.models.matrices.AbstractMatrix.DEFAULT_ROWS;
+import com.lugowoy.tasks.solutions.helper.Helper;
+import com.lugowoy.tasks.solutions.helper.HelperFiller;
 
 /**
  * Transpose square matrix.
- * <p> Created by Konstantin Lugowoy on 29.10.2018.
+ *
+ * <p>Created by Konstantin Lugowoy on 29.10.2018.
  */
 public class Main {
 
     public static void main(String[] args) {
 
-        MatrixInts matrix = new MatrixInts(Helper.FILLING_MATRIX_INTS.fill(DEFAULT_ROWS, DEFAULT_COLUMNS, INT_UPPER_BOUND));
+        MatrixInts matrix = new MatrixInts(new HelperFiller().getArray(
+                new FillingMatrixRandomPrimitiveIntegers(),
+                AbstractMatrix.DEFAULT_ROWS, AbstractMatrix.DEFAULT_COLUMNS,
+                HelperFiller.INT_ZERO, HelperFiller.INT_POSITIVE_TEN));
 
-        System.out.println("Matrix : ");
-        System.out.println(matrix);
+        System.out.println("Matrix : \n" + matrix + "\n");
 
-        for (int i = 0; i < matrix.getRows(); i++) {
-            for (int j = i + 1; j < matrix.getColumns(); j++) {
-                int tmp = matrix.getElement(i, j);
-                matrix.setElement(i, j, matrix.getElement(j, i));
-                matrix.setElement(j, i, tmp);
+        Helper.EXECUTOR.execute(() -> {
+            for (int i = 0; i < matrix.getRows(); i++) {
+                for (int j = i + 1; j < matrix.getColumns(); j++) {
+                    int tmp = matrix.getElement(i, j);
+                    matrix.setElement(i, j, matrix.getElement(j, i));
+                    matrix.setElement(j, i, tmp);
+                }
             }
-        }
 
-        System.out.println("Transpose matrix : ");
-        System.out.println(matrix);
+            System.out.println("Transpose matrix : ");
+            System.out.println(matrix);
+        }, new OutputExecutionTimeToConsole());
 
     }
 
