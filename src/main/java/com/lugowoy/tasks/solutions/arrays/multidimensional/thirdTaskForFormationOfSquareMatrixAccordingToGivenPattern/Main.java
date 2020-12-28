aff2ebf02a62f1f4c0eba1541de62c0fd8fd8f19
Konstantin <kostya.lugowoy@gmail.com>
2020-12-28 16:53:03 +0200

    fix(array) Fixing code in the ../multidimensional/thirdTaskForFormationOfSquareMatrixAccordingToGivenPattern package with a solution of the task

--- src/main/java/com/lugowoy/tasks/solutions/arrays/multidimensional/thirdTaskForFormationOfSquareMatrixAccordingToGivenPattern/Main.java
+++ src/main/java/com/lugowoy/tasks/solutions/arrays/multidimensional/thirdTaskForFormationOfSquareMatrixAccordingToGivenPattern/Main.java
@@ -1,7 +1,9 @@
 package com.lugowoy.tasks.solutions.arrays.multidimensional.thirdTaskForFormationOfSquareMatrixAccordingToGivenPattern;
 
-import com.lugowoy.helper.io.reading.ReadingConsole;
-import com.lugowoy.helper.utils.ArrayLengthReader;
+import com.lugowoy.helper.execution.OutputExecutionTimeToConsole;
+import com.lugowoy.helper.utils.ReaderConsole;
+import com.lugowoy.tasks.solutions.helper.Helper;
+import java.math.BigDecimal;
 
 /**
  * Form a square matrix of order n according to a given pattern:
@@ -11,7 +13,8 @@ import com.lugowoy.helper.utils.ArrayLengthReader;
  * …	…	…	…	…	…	…
  * 0	0	0	…	0	2	0
  * 0	0	0	…	0	0	1
- * <p> Created by Konstantin Lugowoy on 16.10.2017.
+ *
+ * <p>Created by Konstantin Lugowoy on 16.10.2017.
  */
 
 public class Main {
@@ -20,25 +23,29 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Enter the length of the matrix elements : ");
-        int n = ArrayLengthReader.readLength(new ReadingConsole(), UPPER_BOUND);
+        System.out.println("Enter the length of the matrix rows and columns: ");
+        ReaderConsole reader = new ReaderConsole();
+        int n = reader.readInt(BigDecimal.ZERO.intValue(), UPPER_BOUND);
 
-        int[][] array = new int[n][n];
-
+        int[][] matrix = new int[n][n];
         int numIndex = 0, num = n;
-        for (int i = 0; i < array.length; i++) {
-            array[i][numIndex] = num;
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i][numIndex] = num;
             numIndex++;
             num--;
         }
 
-        System.out.println("Result matrix : ");
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                System.out.print(array[i][j] + " ");
+        System.out.println("Matrix : \n" + matrix + "\n");
+
+        Helper.EXECUTOR.execute(() -> {
+            System.out.println("Result matrix : ");
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    System.out.print(matrix[i][j] + " ");
+                }
+                System.out.println();
             }
-            System.out.println();
-        }
+        }, new OutputExecutionTimeToConsole());
 
     }
 
