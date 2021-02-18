12998ed719e20bc3f8bdca924c8ee8603b660020
Konstantin <kostya.lugowoy@gmail.com>
2021-02-18 23:20:36 +0200

    fix(arrays) Fixed code in classes from several packages
    ../arrays/multidimensional/findLargestNumberOfDecreasingElementsOfMatrixGoingInRow
    ../arrays/multidimensional/findLargestNumberOfIncreasingElementsOfMatrixGoingInRow
    ../arrays/multidimensional/findNumberOfAllSaddlePointsOfMatrix

--- src/main/java/com/lugowoy/tasks/solutions/arrays/multidimensional/findNumberOfAllSaddlePointsOfMatrix/Main.java
+++ src/main/java/com/lugowoy/tasks/solutions/arrays/multidimensional/findNumberOfAllSaddlePointsOfMatrix/Main.java
@@ -1,11 +1,14 @@
 package com.lugowoy.tasks.solutions.arrays.multidimensional.findNumberOfAllSaddlePointsOfMatrix;
 
+import com.lugowoy.helper.execution.Executor;
 import com.lugowoy.helper.execution.OutputExecutionTimeToConsole;
 import com.lugowoy.helper.filling.matrix.numbers.primitives.FillingMatrixRandomPrimitiveIntegers;
 import com.lugowoy.helper.models.matrices.MatrixInts;
-import com.lugowoy.tasks.solutions.helper.Helper;
-import com.lugowoy.tasks.solutions.helper.HelperFiller;
+import com.lugowoy.helper.utils.HelperFillerValues;
+import com.lugowoy.helper.utils.HelperMatrixFiller;
+import com.lugowoy.helper.utils.ReaderMatrixLength;
 import org.jetbrains.annotations.NotNull;
+
 import java.util.Objects;
 
 /**
@@ -17,17 +20,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int rows = Helper.readConsoleMatrixRows();
-        int columns = Helper.readConsoleMatrixColumns();
+        ReaderMatrixLength readerMatrixLength = new ReaderMatrixLength();
+        int rows = readerMatrixLength.readRows(System.in, System.out,
+                                               ReaderMatrixLength.MSG_ENTER_MATRIX_ROWS);
+        int columns = readerMatrixLength.readColumns(System.in, System.out,
+                                                     ReaderMatrixLength.MSG_ENTER_MATRIX_COLUMNS);
 
-        MatrixInts matrix = new MatrixInts(new HelperFiller().getArray(
+        HelperMatrixFiller fillerMatrix = new HelperMatrixFiller();
+        MatrixInts matrix = new MatrixInts(fillerMatrix.getArray(
                 new FillingMatrixRandomPrimitiveIntegers(), rows, columns,
-                HelperFiller.INT_ZERO, HelperFiller.INT_POSITIVE_HUNDRED));
+                HelperFillerValues.INT_ZERO,
+                HelperFillerValues.INT_POSITIVE_HUNDRED));
 
         System.out.println("Matrix : \n" + matrix + "\n");
 
-        Helper.EXECUTOR.execute(() -> findSaddleNumbersOfMatrix(matrix),
-                                new OutputExecutionTimeToConsole());
+        new Executor().execute(() -> findSaddleNumbersOfMatrix(matrix),
+                               new OutputExecutionTimeToConsole());
 
     }
 
@@ -66,9 +74,9 @@ public class Main {
         return result;
     }
 
-    private static boolean isMaxElementInColumn(@NotNull final MatrixInts matrix,
-                                                final int indexColumn,
-                                                final int valueToEqual) {
+    private static boolean isMaxElementInColumn(
+            @NotNull final MatrixInts matrix, final int indexColumn,
+            final int valueToEqual) {
         boolean result = true;
         for (int i = 0; i < matrix.getRows(); i++) {
             int element = matrix.getElement(i, indexColumn);
